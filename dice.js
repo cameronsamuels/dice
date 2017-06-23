5b648d647760543fcf8d2a2cf0bfe05b9108a112
Cameron Samuels <satchmosanders@gmail.com>
2017-06-23 14:10:49 -0500

    Cleaned up code

--- dice.js
+++ dice.js
@@ -1,19 +1,18 @@
-function id(id) { return document.getElementById(id); }
-var snd = new Audio('roll.mp3'), colors = ['blue', 'yellow', 'green', 'red', 'purple', 'orange'];
-function update() {
-    var num = Math.ceil(Math.random() * 6);
-    id("dice").setAttribute("class", "");
-    id('dice').src = 'img/' + num + 't.png';
-    document.body.style.backgroundColor = colors[num - 1];
+var $ = function(e) { return document.getElementById(e) }, a = new Audio('roll.mp3');
+a.load();
+function b() {
+    var d = Math.ceil(Math.random() * 6);
+    $("dice").setAttribute("class", "");
+    $('dice').src = 'img/' + d + 't.png';
 }
-function roll() {
-    if (!id('dice').getAttribute('class').includes('anim')) {
-        id('dice').setAttribute('class', 'anim');
-        snd.play();
+function c() {
+    if (!$('dice').getAttribute('class').includes('anim')) {
+        $('dice').setAttribute('class', 'anim');
+        a.play();
         setTimeout('update()', 1000);
     }
 }
-if (!navigator.userAgent.match(/Android/i) && !navigator.userAgent.match(/iPhone|iPad|iPod/i)) document.body.onclick = roll;
-document.body.onkeydown = roll;
+if (!navigator.userAgent.match(/Android/i) && !navigator.userAgent.match(/iPhone|iPad|iPod/i)) document.body.onclick = c;
+document.body.onkeydown = c;
 document.body.ontouchmove = function(e){e.preventDefault()};
-update();
+b();
