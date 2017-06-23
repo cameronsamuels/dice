af929dedbad3a629d62b9ac624658df57349b2b0
CameronSamuels <satchmosanders@gmail.com>
2017-06-23 15:46:02 -0500

    Minified d.js

--- d.js
+++ d.js
@@ -1,17 +1,1 @@
-var $ = document.querySelector('img'), a = new Audio('a/r.mp3');
-a.load();
-function b() {
-    var d = Math.ceil(Math.random() * 6);
-    $.className = "";
-    $.src = 'a/' + d + '.svg';
-}
-function c() {
-    if (!$.className) {
-        $.className = 'a';
-        a.play();
-        setTimeout(b, 1000);
-    }
-}
-if (!navigator.userAgent.match(/Android/i) && !navigator.userAgent.match(/iPhone|iPad|iPod/i)) document.onclick = c;
-document.onkeydown = c, document.ontouchmove = function(e){e.preventDefault()};
-b()
+function b(){var a=Math.ceil(6*Math.random());$.className="",$.src="a/"+a+".svg"}function c(){$.className||($.className="a",a.play(),setTimeout(b,1e3))}var $=document.querySelector("img"),a=new Audio("a/r.mp3");a.load(),navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/iPhone|iPad|iPod/i)||(document.onclick=c),document.onkeydown=c,document.ontouchmove=function(a){a.preventDefault()},b();
