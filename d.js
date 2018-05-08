a1ca66827e2b9619e5cdfe083fb6aa8213741ddb
Cameron Samuels <me@cameronsamuels.com>
2018-05-07 20:42:43 -0500

    (update)Beautified and cleaned js

--- d.js
+++ d.js
@@ -1,1 +1,23 @@
-function b(){var a=Math.ceil(6*Math.random());$.className="",$.src="a/"+a+".svg"}function c(e){e.preventDefault();$.className||($.className="a",a.play(),setTimeout(b,1e3))}var $=document.querySelector("img"),a=new Audio("a/r.mp3");a.load(),navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)?document.ontouchstart=c:document.onclick=c,document.onkeydown=c,document.ontouchmove=function(a){a.preventDefault()},document.oncontextmenu=function(a){a.preventDefault()},document.querySelector('img').ondragstart=function(){return false},b()
+var $ = document.querySelector("img");
+var a = new Audio("a/r.mp3");
+
+function b() {
+  var a = Math.ceil(6 * Math.random());
+  $.className = "";
+  $.src = "a/" + a + ".svg";
+}
+
+function c(e) {
+  e.preventDefault();
+  if ($.className) return;
+  $.className = "a";
+  a.play();
+  setTimeout(b, 1000);
+}
+
+document.onkeydown = c;
+if (navigator.userAgent.match(/iPhone|iPad|iPod|Android/i)) document.ontouchstart = c;
+else document.onclick = c;
+
+a.load();
+b();
