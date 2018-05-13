02fe73bf81ed18b9fce7b0d4a7601a8434ed308c
Cameron Samuels <me@cameronsamuels.com>
2018-05-13 16:10:00 -0500

    (update)Updated the app (#18)
    * Delete icon.svg
    
    * Delete ipad-3.png
    
    * Delete ipad-5.png
    
    * Delete ipad-6.png
    
    * Delete ipad-widget.png
    
    * Delete iphone-3-6.jpg
    
    * Delete iphone-5.jpg
    
    * Delete iphone-widget.jpg
    
    * Delete chrome1.png
    
    * Delete chrome2.png
    
    * Delete chrome3.png
    
    * Delete large-tile.png
    
    * Delete marquee.png
    
    * Delete small-tile.png
    
    * Update and rename d.css to css/style.css
    
    * Update and rename d.js to js/script.js
    
    * Update index.html
    
    * Delete r.mp3
    
    * Add files via upload
    
    * Delete r.mp3
    
    * Delete manifest.json
    
    * Delete b.png
    
    * Delete d.css
    
    * Delete d.js
    
    * Delete index.html
    
    * Delete 6.svg
    
    * Delete 2.svg
    
    * Delete 3.svg
    
    * Delete 4.svg
    
    * Delete 5.svg
    
    * Delete 1.svg

--- webext/d.js
+++
@@ -1,1 +0,0 @@
-function b(){var a=Math.ceil(6*Math.random());$.className="",$.src="a/"+a+".svg"}function c(){$.className||($.className="a",a.play(),setTimeout(b,1e3))}var $=document.querySelector("img"),a=new Audio("r.mp3");document.onkeydown=c,document.onclick=c,b()
